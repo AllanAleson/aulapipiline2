a3e7f5e7173cd83ea507ddf15e518d6736622820
Allan Aleson <chrollo.lucilfer2017@gmail.com>
2026-04-01 10:36:14 -0300

    adicionando mais testes

--- tests/soma.test.js
+++ tests/soma.test.js
@@ -1,5 +1,20 @@
 const soma = require('../src/soma');
+const subtracao = require('../src/subtracao');
+const multiplicacao = require('../src/multiplicacao');
+const divisao = require('../src/divisao');
 
 test('soma 2 + 3', () => {
   expect(soma(2, 3)).toBe(5);
+});
+
+test('subtracao 5 - 3', () => {
+  expect(subtracao(5, 3)).toBe(2);
+});
+
+test('multiplicacao 4 * 3', () => {
+  expect(multiplicacao(4, 3)).toBe(12);
+});
+
+test('divisao 10 / 2', () => {
+  expect(divisao(10, 2)).toBe(5);
 });
